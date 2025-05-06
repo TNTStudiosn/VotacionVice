ae0e725249a7ed8df78d15b8364cd2f50897097a
TNTStudios <tntstudiosn@gmail.com>
2025-05-05 23:45:52 -0600

    Initial commit

---
+++ src/client/java/com/TNTStudios/votacionvice/client/VotacionviceClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.votacionvice.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VotacionviceClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/votacionvice/Votacionvice.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.votacionvice;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Votacionvice implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
